fab3d7c8415cafa34c1b8b6aa7a11643bbe88ce5
Razvan Vuscan <razvan.vuscan@xoomworks.com>
2013-03-25 11:44:32 +0300

    * Modified the check error message methods, so that now the error stored in a string is correctly taken from the webpage.
    * Added a "Click Create New Account" button method in the
    LoginPageHelper.
    
    * Added a "Create new account" key in the UI Map properties document.

--- selenium_mvn_eclipse/src/test/java/ean/poc/helpers/LoginPageHelper.java
+++ selenium_mvn_eclipse/src/test/java/ean/poc/helpers/LoginPageHelper.java
@@ -18,4 +18,10 @@ public class LoginPageHelper {
 	public void clickLogin(WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.login.button.loginUser"))).click();
 	}
+	
+	
+	//Method used to click the "Create new account" button in order to reach the "Create account" page.
+	public void clickNewAccount(WebDriver driver) throws Exception {
+		driver.findElement(By.xpath(UIMap.find("ean.poc.login.button.createAccount"))).click();
+	}
 }
