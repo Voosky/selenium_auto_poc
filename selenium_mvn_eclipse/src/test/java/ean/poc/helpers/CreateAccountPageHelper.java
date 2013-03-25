c36f0cf634685070e05ff637973d82eb73015073
Razvan Vuscan <razvan.vuscan@xoomworks.com>
2013-03-25 18:31:42 +0300

    * Added descriptions and description updates for the methods in the following classes: UIMap, CreateAccountPage, LoginPageHelper.
    * Added an ean.poc.login.successMessage.accountCreated key in the
    messages.properties file.
    
    * Added ean.poc.login.form.creationSuccessField and
    ean.poc.login.form.errorField keys in the uimap.properties file.

--- selenium_mvn_eclipse/src/test/java/ean/poc/helpers/CreateAccountPageHelper.java
+++ selenium_mvn_eclipse/src/test/java/ean/poc/helpers/CreateAccountPageHelper.java
@@ -10,38 +10,53 @@ import ean.poc.UIMap;
 
 public class CreateAccountPageHelper {
 	
-	//Method used to set the user name in the Username field.
+	/*Method used to type the desired Username into the Username input field (Create Account page).  
+	* @username - the value of the desired username string used.
+	* @ean.poc.createAccount.inputField.username - the XPath location of the username input field.
+	*/
 	public void setUsername(String username, WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.inputField.username"))).sendKeys(username);
 	}
 	
 	
-	//Method used to set the E-mail in the E-mail input field.
+	/*Method used to type the desired E-mail into the E-mail input field (Create Account page).  
+	* @email - the value of the desired e-mail string used.
+	* @ean.poc.createAccount.inputField.email - the XPath location of the e-mail input field.
+	*/
 	public void setEmail(String email, WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.inputField.email"))).sendKeys(email);
 	}
 	
 	
-	//Method used to set the password in the Password field.
+	/*Method used to type the desired Password into the Password input field (Create Account page).  
+	* @password - the value of the desired Password string used.
+	* @ean.poc.createAccount.inputField.password - the XPath location of the Password input field.
+	*/
 	public void setPassword(String password, WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.inputField.password"))).sendKeys(password);
 	}
 	
 	
-	//Method used to send a password in the "Verify Password" field.
-	public void checkPassword(String verifyPassword, WebDriver driver) throws Exception {
+	/*Method used to type the verification password into the Verify Password input field (Create Account page).  
+	* @verifyPassword - the value of the desired verification password string used.
+	* @ean.poc.createAccount.inputField.verifyPassword - the XPath location of the Verify Password input field.
+	*/
+	public void setVerificationPassword(String verifyPassword, WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.inputField.verifyPassword"))).sendKeys(verifyPassword);
 	}
 	
 	
-	//Method used to click the "Create" button in the "Create User" form. 
+	//Method used to click the "Create" button on the "Create Account" page. 
 	public void clickCreate(WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.button.create"))).click();	
 	}
 	
 	
-	//Method used to verify the User Name field in terms of length(minimum and maximum), 
-	//supported characters, and null/blank values. 
+	/*Method used to verify the User Name field in terms of length(minimum and maximum), 
+	* supported characters, and null/blank values. 
+	* @ean.poc.createAccount.form.errorField - the XPath location of the error field on the Create Account page.
+	* @ean.poc.createAccount.errorMessage.invalidUserName - retrieves the value of the "Invalid Username" error message Key. 
+	*/
 	public void verifyUsernameError(WebDriver driver) throws Exception {
 		String errorMessage = driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.form.errorField"))).getText();
 		
@@ -49,8 +64,11 @@ public class CreateAccountPageHelper {
 	}
 	
 	
-	//Method used to verify the Password field in terms of length(minimum), and null values. 
-	//The method does not cover special character situations for now! (due to limitations in the software)
+	/*Method used to verify the Password field in terms of length(minimum), and null values. 
+	* The method does not cover special character situations for now! (due to limitations in the software)
+	* @ean.poc.createAccount.form.errorField - the XPath location of the error field on the Create Account page.
+	* @ean.poc.createAccount.errorMessage.invalidPassword - retrieves the value of the "Invalid Password" error message Key. 
+	*/
 	public void verifyPasswordError(WebDriver driver) throws Exception {
 		String errorMessage = driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.form.errorField"))).getText();
 		
@@ -58,8 +76,11 @@ public class CreateAccountPageHelper {
 	}
 	
 	
-	//Method used to check the Verify Password field in terms of length(lower or higher), 
-	//different characters than in the Password field, and null values. 
+	/*Method used to check the Verify Password field in terms of length(lower or higher), 
+	* different characters than in the Password field, and null values (mismatch). 
+	* @ean.poc.createAccount.form.errorField - the XPath location of the error field on the Create Account page.
+	* @ean.poc.createAccount.errorMessage.passwordMismatch - retrieves the value of the "Password Mismatch" error message Key. 
+	*/ 
 	public void verifyMatchingPasswordError(WebDriver driver) throws Exception {
 		String errorMessage = driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.form.errorField"))).getText();
 		
@@ -67,20 +88,14 @@ public class CreateAccountPageHelper {
 	}
 	
 	
-	//Method used to verify the Email field in terms of correct format (example@example.com).
+	/*Method used to verify the Email field in terms of correct format (example@example.com).
+	* @ean.poc.createAccount.form.errorField - the XPath location of the error field on the Create Account page.
+	* @ean.poc.createAccount.errorMessage.invalidEmail - retrieves the value of the "Invalid Email" error message Key. 
+	*/ 
 	public void verifyEmail(WebDriver driver) throws Exception {
 		String errorMessage = driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.form.errorField"))).getText();
 		
 		Assert.assertEquals(errorMessage, MessageMap.find("ean.poc.createAccount.errorMessage.invalidEmail"));
 	}
-	
-	
-	//Method used to verify that a specific message is returned when the user creates a new account.
-	public void verifySuccessfulCreation(WebDriver driver) throws Exception {
-		String errorMessage = driver.findElement(By.xpath(UIMap.find("ean.poc.createAccount.form.errorField"))).getText();
 		
-		Assert.assertEquals(errorMessage, MessageMap.find("ean.poc.createAccount.successMessage.success"));
-	}
-	
-	
 }
--- selenium_mvn_eclipse/src/test/java/ean/poc/helpers/LoginPageHelper.java
+++ selenium_mvn_eclipse/src/test/java/ean/poc/helpers/LoginPageHelper.java
@@ -1,27 +1,69 @@
 package ean.poc.helpers;
 
+import junit.framework.Assert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import ean.poc.MessageMap;
 import ean.poc.UIMap;
 
 public class LoginPageHelper {
 
+	/*Method used to type the Username into the Username input field (Log-In page).  
+	* @username - the value of the username string which will be used.
+	* @ean.poc.login.inputField.username - the XPath location of the username input field.
+	*/
 	public void setUsername(String username, WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.login.inputField.username"))).sendKeys(username);
 	}
 	
+	
+	/*Method used to type the Password into the Password input field (Log-In page).  
+	* @password - the value of the password string which will be used.
+	* @ean.poc.login.inputField.password - the XPath location of the password input field.
+	*/
 	public void setPassword(String password, WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.login.inputField.password"))).sendKeys(password);
 	}
 	
+	
+	/*Method used to click the "Login" button on the Log-In page.  
+	* @ean.poc.login.button.loginUser - the XPath location of the Login button.
+	*/
 	public void clickLogin(WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.login.button.loginUser"))).click();
 	}
 	
 	
-	//Method used to click the "Create new account" button in order to reach the "Create account" page.
+	/*Method used to click the "Create new account" button in order to reach the "Create account" page.
+	* @ean.poc.login.button.createAccount - the XPath location of the Create Account button.
+	*/	
 	public void clickNewAccount(WebDriver driver) throws Exception {
 		driver.findElement(By.xpath(UIMap.find("ean.poc.login.button.createAccount"))).click();
+	}
+	
+	
+	/*Method used to verify that a success message is displayed in the log-in page after creating a new account.
+	* @ean.poc.login.form.creationSuccess - the XPath location of the error field on the Create Account page.
+	* @ean.poc.createAccount.errorMessage.invalidEmail - retrieves the value of the "Invalid Email" error message Key. 
+	*/ 
+	public void verifySuccessfulCreation(WebDriver driver) throws Exception {
+		String errorMessage = driver.findElement(By.xpath(UIMap.find("ean.poc.login.form.creationSuccessField"))).getText();
+		
+		Assert.assertEquals(errorMessage, MessageMap.find("ean.poc.login.successMessage.accountCreated"));
+	}
+	
+	
+	/*Method used to verify the error messages displayed in the log-in page.   
+	* Cases covered: default/blank values (for username, password, or both),
+	* invalid username/password (inexistent, incorrect credentials, lower/higher number of characters). 
+	* @ean.poc.login.form.errorField - the XPath location of the error field on the Log-In page.
+	* @ean.poc.login.errorMessage.invalidUserPass - retrieves the value of the "Invalid Username or Password" error message Key. 
+	*/
+	public void verifyInvalidUserPass (WebDriver driver) throws Exception {
+		String errorMessage = driver.findElement(By.xpath(UIMap.find("ean.poc.login.form.errorField"))).getText();
+		
+		Assert.assertEquals(errorMessage, MessageMap.find("ean.poc.login.errorMessage.invalidUserPass"));
 	}
 }
